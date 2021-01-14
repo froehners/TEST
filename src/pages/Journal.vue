26a957bee1b5762b2fc2d2aaeb92d758b56469a5
Stephanie <sfroehner@gmail.com>
2021-01-13 22:44:51 -0800

    Update Journal.vue
    journal h1/hero title

--- src/pages/Journal.vue
+++ src/pages/Journal.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="journal-hero">
         <h1 class="journal-header">
-          a wise person once said...
+          “The things that we love tell us who we are.” 
         </h1>
       </div>
     </div>
@@ -51,7 +51,7 @@ export default {
 .journal-hero {
   padding: 4rem 0;
   text-align: center;
-  color: var(--color-base-1);
+  color: var(--color-contrast);
 }
 .journal-header {
   font-size: 3rem;
