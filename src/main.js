9dbeb47674eec41adf191e9f4b11e5b4f4f0b01a
Stephanie <sfroehner@gmail.com>
2021-02-17 18:43:17 -0800

    Update main.js
    Adding CDN font

--- src/main.js
+++ src/main.js
@@ -9,3 +9,10 @@ export default function(Vue, { head }) {
     class: settings.dark_mode ? "dark" : ""
   };
 }
+
+export default function (Vue, { head }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,500;0,700;1,500;1,700&family=Pirata+One&display=swap'
+  })
+}
