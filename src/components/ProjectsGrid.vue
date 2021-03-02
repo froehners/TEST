d6008b0e8e5ac6158207ab69f4f08e2fa23bf858
Stephanie <sfroehner@gmail.com>
2021-03-01 20:37:23 -0800

    Update ProjectsGrid.vue

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -47,6 +47,7 @@ export default {
   box-shadow: 0 0 40px -20px rgba(0,0,0,0.25);
 }
 .project-title {
+  font-family: 'Pirata One', cursive;
   font-size: 2rem;
   color: var(--color-contrast);
   margin: 2rem 0 1rem 0;
